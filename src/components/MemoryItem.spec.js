a29bdd35db271d868c2f240e358e8e32a06d9025
Danica Dombek <danica.dombek@gmx.de>
2021-06-07 17:53:54 +0200

    Update MemoryItem.spec with image-test

--- src/components/MemoryItem.spec.js
+++ src/components/MemoryItem.spec.js
@@ -5,5 +5,6 @@ describe('MemoryItem', () => {
   it('renders a subtitle', () => {
     render(<MemoryItem subtitle="Memory 1" />)
     expect(screen.getByText('Memory 1')).toBeInTheDocument()
+    expect(screen.getByRole('img')).toBeInTheDocument()
   })
 })
