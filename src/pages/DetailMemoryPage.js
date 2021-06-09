fa7160059a14702220eabe70724fec2a8ab1757e
Danica Dombek <danica.dombek@gmx.de>
2021-06-10 01:34:30 +0200

    Add styling to DetailMemoryPage.js and onClick to go back to Overview

--- src/pages/DetailMemoryPage.js
+++ src/pages/DetailMemoryPage.js
@@ -1,19 +1,37 @@
 import styled from 'styled-components/macro'
 import Button from '../components/Button'
-import Header from '../components/Header'
+import PropTypes from 'prop-types'
 
-export default function DetailMemoryPage({ image }) {
+DetailMemoryPage.propTypes = {
+  image: PropTypes.node,
+  onNavigate: PropTypes.func.isRequired,
+}
+
+export default function DetailMemoryPage({ image, onNavigate }) {
   return (
     <Wrapper>
-      <Header>Viary</Header>
-      {image}
-      <Button>Back to memories</Button>
+      <Image src={image} alt="" width="95%" />
+      <nav>
+        <ButtonStyle onClick={onNavigate}>Back to memories</ButtonStyle>
+      </nav>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-rows: 80px 350px 40px;
-  justify-content: center;
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 86vh;
+`
+const Image = styled.img`
+  border-radius: 20px;
+  box-shadow: 5px 5px 15px 5px #6d6d6d;
+  margin-top: 20px;
+`
+
+const ButtonStyle = styled(Button)`
+  left: 150px;
+  bottom: 20px;
 `
