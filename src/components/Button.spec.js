23b6d8026944f02d1c41c78b05b7a7c9983224ae
Danica Dombek <danica.dombek@gmx.de>
2021-06-09 13:38:20 +0200

    Write test for button-component

--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,10 +1,22 @@
-import {render, screen} from '@testing-library/react'
-  import Button from './Button'
-    
-  describe('Button', () => {
-      it('renders', () => {
-          render(<Button />)
-          expect(screen.getByText('Button')).toBeInTheDocument()
-      })
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import Button from './Button'
+
+describe('Button', () => {
+  it('contains a text', () => {
+    render(<Button onClick={() => {}}>Click me</Button>)
+
+    const button = screen.getByRole('button', { name: 'Click me' })
+    expect(button).toBeInTheDocument()
   })
-        
+
+  it('calls onClick', async () => {
+    const handleClick = jest.fn()
+    render(<Button onClick={handleClick}>Click me</Button>)
+
+    const button = screen.getByRole('button', { name: 'Click me' })
+    userEvent.click(button)
+
+    expect(handleClick).toHaveBeenCalledTimes(1)
+  })
+})
