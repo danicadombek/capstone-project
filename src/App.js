dc326b6a144dacb63366e10fe974e75611dd2b09
Danica Dombek <danica.dombek@gmx.de>
2021-06-10 01:29:51 +0200

    Set useState for currentPages in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import background from '../src/assets/viary-bg.jpg'
+import { useState } from 'react'
+import background from '../src/assets/viary-bg2.jpg'
 import Header from '../src/components/Header'
 import MemoriesPage from './pages/MemoriesPage'
+import DetailMemoryPage from './pages/DetailMemoryPage'
 import memory1 from './assets/images/memory1.jpg'
 import memory2 from './assets/images/memory2.jpg'
 import memory3 from './assets/images/memory3.jpg'
 
 export default function App() {
+  const [currentPage, setCurrentPage] = useState('memories')
+
   const listOfMemories = [
     { image: memory1, subtitle: 'Memory 1' },
     { image: memory2, subtitle: 'Memory 2' },
@@ -26,7 +30,22 @@ export default function App() {
       }}
     >
       <Header>Viary</Header>
-      <MemoriesPage memories={listOfMemories} />
+      {currentPage === 'memories' && (
+        <MemoriesPage
+          memories={listOfMemories}
+          onDetail={showDetailMemoryPage}
+        />
+      )}
+      {currentPage === 'detail' && (
+        <DetailMemoryPage image={memory1} onNavigate={showMemoriesPage} />
+      )}
     </div>
   )
+  function showDetailMemoryPage() {
+    setCurrentPage('detail')
+  }
+
+  function showMemoriesPage() {
+    setCurrentPage('memories')
+  }
 }
