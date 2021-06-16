ff1746cd2c901aa8183d4491161d016fe56feba9
Danica Dombek <danica.dombek@gmx.de>
2021-06-16 11:27:36 +0200

    Install and prepare cloudinary

--- src/pages/CameraPage.js
+++ src/pages/CameraPage.js
@@ -1,71 +1,15 @@
-import React, { useEffect, useRef } from 'react'
 import styled from 'styled-components/macro'
 import Button from '../components/Button'
 import ToMemoriesButton from '../components/ToMemoriesButton'
 import CamIcon from '../assets/images/icons/cam.png'
 
 export default function CameraPage() {
-  const videoRef = useRef(null)
-  const pictureRef = useRef(null)
-  const stripRef = useRef(null)
-
-  useEffect(() => {
-    getVideo()
-  }, [videoRef])
-
-  const getVideo = () => {
-    navigator.mediaDevices
-      .getUserMedia({ video: { width: 250 } })
-      .then(stream => {
-        let video = videoRef.current
-        video.srcObject = stream
-        video.play()
-      })
-      .catch(err => {
-        console.error('error:', err)
-      })
-  }
-
-  const paintToCanvas = () => {
-    let video = videoRef.current
-    let picture = pictureRef.current
-    let compress = picture.getContext('2D')
-
-    const width = 250
-    const height = 400
-    picture.width = width
-    picture.height = height
-
-    return setInterval(() => {
-      compress.drawImage(video, 0, 0, width, height)
-    }, 200)
-  }
-
-  const takePicture = () => {
-    let picture = pictureRef.current
-    let strip = stripRef.current
-
-    console.warn(strip)
-
-    const data = picture.toDataURL('image/jpeg')
-
-    console.warn(data)
-    const link = document.createElement('a')
-    link.href = data
-    link.setAttribute('download', 'myWebcam')
-    link.innerHTML = `<img scr='${data}' alt='thumbnail'/>`
-    strip.insertBefore(link, strip.firstChild)
-  }
-
   return (
     <Wrapper>
-      <CamButton onClick={() => takePicture()}>
+      <CamButton>
         <img src={CamIcon} alt="" />
         <span>Cam</span>
       </CamButton>
-      <video onCanPlay={() => paintToCanvas()} ref={videoRef} />
-      <canvas ref={pictureRef} />
-      <div ref={stripRef} />
       <h2>Start your cam</h2>
       <ToMemoriesButton>To your memories</ToMemoriesButton>
     </Wrapper>
