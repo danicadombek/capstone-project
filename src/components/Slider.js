a3009d77bb76075426a571441eaaa3e1b977a69b
Danica Dombek <danica.dombek@gmx.de>
2021-06-30 21:57:28 +0200

    Correct slider-component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -33,7 +33,6 @@ export default function ImageSlider({
           }}
         />
       ))}
-      <Gradient />
     </Wrapper>
   )
 }
@@ -53,12 +52,4 @@ const Slide = styled.div`
   height: 55%;
   transition: 800ms all ease-in-out;
   width: 100%;
-`
-
-const Gradient = styled.div`
-  height: 50%;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 90%;
 `
